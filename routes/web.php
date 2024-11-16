37af176fe7263beab1402156b663feaaf1dc0a42
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-11-16 15:39:49 +0545

    FIrstPush_Laravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function(){
+
+    return view('contact');
+});
+
